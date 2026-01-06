8ea509f0230f343b81bd8b2539697c4e1d855506
Tu Nombre <tu@email>
2026-01-06 15:46:09 -0400

    auto-push 2026-01-06 15:46:08

--- app/models_auth.py
+++ app/models_auth.py
@@ -5,20 +5,6 @@ import datetime as dt
 from app.extensions import db
 
 
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True, index=True, nullable=False)
-    name = db.Column(db.String(120), nullable=True)
-
-    password_hash = db.Column(db.String(255), nullable=False)
-    is_verified = db.Column(db.Boolean, default=False, nullable=False)
-
-    created_at = db.Column(db.DateTime, default=dt.datetime.utcnow, nullable=False)
-    updated_at = db.Column(db.DateTime, default=dt.datetime.utcnow, nullable=False, onupdate=dt.datetime.utcnow)
-
-
 class EmailVerificationToken(db.Model):
     __tablename__ = "email_verification_tokens"
 
